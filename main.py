46cadfd2c350e18e3c1140377aeff595eb3b4c57
Thoosje <ikke@thomasnelissen.be>
2021-10-26 22:23:44 +0200

    First commit - Initialized repo

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+class IncapsulaGen():
+    def __init__(self, session: requests.Session, script: str, user_data: dict[str, Any]) -> None:
+        self._session: requests.Session = requests.Session()
+        self.script: str = script
+    
+    def create_data(self) -> None:
